dbc01d16098cb8b2ef659eb05542975e59cfb9c9
studmanj <bjackso3@hotmail.com>
2022-08-29 17:05:59 -0600

    Add CSS and classNames

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,13 @@
-
-
 function Navbar() {
-    return (
-        <h1>Navbar goes here</h1>
-    )
+  return (
+    <nav>
+      <img src="./images/react.png" 
+           alt="React Logo" 
+           className="nav--icon" />
+      <h3 className="nav--logo">ReactFacts</h3>
+      <h4 className="nav--title">React Course - Project 1</h4>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
