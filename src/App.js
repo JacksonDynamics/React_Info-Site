626128c41194c4a733d9fb4884ea3e03b6e54f77
studmanj <bjackso3@hotmail.com>
2023-01-05 23:56:08 -0700

    useState

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
+import React, {useState} from "react"
 import Navbar from "./components/Navbar"
 import Main from "./components/Main"
 
 function App() {
+  const [darkMode, setDarkMode] = useState(true)
+  
+  function toggleDarkMode() {
+    setDarkMode(prevMode => !prevMode)
+  }
+  
   return (
     <div className="container">
-      <Navbar />
-      <Main />
-    </div>
-  )
-}
+            <Navbar 
+                darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+            <Main darkMode={darkMode} />
+        </div>
+    )
+  }
 
-export default App;
-
+  export default App
