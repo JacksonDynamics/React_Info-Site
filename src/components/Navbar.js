c710015bc6b2f94faf01ee78182435b9e4b1aaf0
studmanj <bjackso3@hotmail.com>
2023-01-05 23:59:46 -0700

    Navbar toggle

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,28 @@
-function Navbar() {
+function Navbar(props) {
   return (
-    <nav>
-      <img src="./images/react.png" 
-           alt="React Logo" 
-           className="nav--icon" />
-      <h3 className="nav--logo">ReactFacts</h3>
-      <h4 className="nav--title">React Course - Project 1</h4>
-    </nav>
-  );
+       <nav 
+          className={props.darkMode ? "dark": ""}
+      >
+          <img 
+              className="nav--logo_icon"
+              src="./images/react-icon-small.png"
+          />
+          <h3 className="nav--logo_text">ReactFacts</h3>
+          
+          <div 
+              className="toggler" 
+          >
+              <p className="toggler--light">Light</p>
+              <div 
+                  className="toggler--slider"
+                  onClick={props.toggleDarkMode}
+              >
+                  <div className="toggler--slider--circle"></div>
+              </div>
+              <p className="toggler--dark">Dark</p>
+          </div>
+      </nav>
+  )
 }
 
 export default Navbar;
